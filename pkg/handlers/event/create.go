a2d929326b474b034dba06ec65f406039a1a9560
Vadym Tyemirov <temirov@gmail.com>
2025-11-03 01:34:59 -0800

    refactor: rewrite module namespace after owner rename

--- pkg/handlers/event/create.go
+++ pkg/handlers/event/create.go
@@ -7,11 +7,11 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/temirov/RSVP/models"
-	"github.com/temirov/RSVP/pkg/config"
-	"github.com/temirov/RSVP/pkg/handlers"
-	"github.com/temirov/RSVP/pkg/middleware"
-	"github.com/temirov/RSVP/pkg/utils"
+	"github.com/tyemirov/RSVP/models"
+	"github.com/tyemirov/RSVP/pkg/config"
+	"github.com/tyemirov/RSVP/pkg/handlers"
+	"github.com/tyemirov/RSVP/pkg/middleware"
+	"github.com/tyemirov/RSVP/pkg/utils"
 	"gorm.io/gorm"
 )
 
